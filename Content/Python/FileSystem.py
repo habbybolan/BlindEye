8a8e61ae059c2b175279a5d073abac4d29fea877
pg22nicholas <97759807+pg22nicholas@users.noreply.github.com>
2022-08-29 15:06:04 -0700

    Added python scripts with updated gitignore to prevent cache files

---
+++ Content/Python/FileSystem.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:160e068dc39d6cb8a03fc20243c7f14487dc70678b53ce237bd66f6a360daf99
+size 692
---
+++ Content/Python/ImportedAssetData.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:14a331db97d2554c404637da8074d556ccbe057b500716ebb3a49df5c7897724
+size 8112
