d1c8f5992ccf16a2914c8730b1416202a96c78a0
pg22nicholas <97759807+pg22nicholas@users.noreply.github.com>
2022-08-30 17:24:08 -0700

    Added base damage type with stun and knockBack

---
+++ Source/BlindEye/Private/DamageTypes/KnockBackDamageType.cpp
@@ -0,0 +1,11 @@
+// Copyright (C) Nicholas Johnson 2022
+
+
+#include "DamageTypes/KnockBackDamageType.h"
+
+float UKnockBackDamageType::ProcessDamage(AActor* Owner, APawn* HitCharacter, FVector HitLocation) const
+{
+
+	return Super::ProcessDamage(Owner, HitCharacter, HitLocation);
+	// TODO:
+}
