71f9a6308bdb2cf49c1c655c27fac30e73235a83
pg22nicholas <pg22nicholas@vfs.com>
2022-09-01 23:03:21 -0700

    Added Health and Bird meter properties to player state

--- Source/BlindEye/Private/Gameplay/BlindEyePlayerState.cpp
+++ Source/BlindEye/Private/Gameplay/BlindEyePlayerState.cpp
@@ -3,3 +3,37 @@
 
 #include "Gameplay/BlindEyePlayerState.h"
 
+#include "Net/UnrealNetwork.h"
+
+ABlindEyePlayerState::ABlindEyePlayerState()
+{
+	CurrHealth = MaxHealth;
+	CurrBirdMeter = MaxBirdMeter;
+}
+
+float ABlindEyePlayerState::GetHealth()
+{
+	return CurrHealth;
+}
+
+void ABlindEyePlayerState::SetHealth(float NewHealth)
+{
+	CurrHealth = NewHealth;
+}
+
+float ABlindEyePlayerState::GetBirdMeter()
+{
+	return CurrBirdMeter;
+}
+
+void ABlindEyePlayerState::SetBirdMeter(float NewBirdMeter)
+{
+	CurrBirdMeter = NewBirdMeter;
+}
+
+void ABlindEyePlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ABlindEyePlayerState, CurrHealth);
+	DOREPLIFETIME(ABlindEyePlayerState, CurrBirdMeter);
+}
--- Source/BlindEye/Public/Gameplay/BlindEyePlayerState.h
+++ Source/BlindEye/Public/Gameplay/BlindEyePlayerState.h
@@ -13,5 +13,28 @@ UCLASS()
 class BLINDEYE_API ABlindEyePlayerState : public APlayerState
 {
 	GENERATED_BODY()
+
+public:
+	ABlindEyePlayerState();
+
+	float GetHealth();
+	void SetHealth(float NewHealth);
+
+	float GetBirdMeter();
+	void SetBirdMeter(float NewBirdMeter);
+
+protected:
+	virtual void GetLifetimeReplicatedProps( TArray< FLifetimeProperty > & OutLifetimeProps ) const override;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	float MaxHealth = 100.f;
+	UPROPERTY(Replicated)
+	float CurrHealth;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	float MaxBirdMeter = 100.f;
+	UPROPERTY(Replicated)
+	float CurrBirdMeter;
+	
 	
 };
