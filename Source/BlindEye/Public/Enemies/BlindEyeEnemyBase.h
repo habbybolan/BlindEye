236861b1f302daae2f4639cbc90a1fe6a28329bf
pg22nicholas <97759807+pg22nicholas@users.noreply.github.com>
2022-09-02 12:38:09 -0700

    Added Health interface to enemy

--- Source/BlindEye/Public/Enemies/BlindEyeEnemyBase.h
+++ Source/BlindEye/Public/Enemies/BlindEyeEnemyBase.h
@@ -4,10 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Interfaces/HealthInterface.h"
 #include "BlindEyeEnemyBase.generated.h"
 
 UCLASS()
-class BLINDEYE_API ABlindEyeEnemyBase : public ACharacter
+class BLINDEYE_API ABlindEyeEnemyBase : public ACharacter, public IHealthInterface
 {
 	GENERATED_BODY()
 
@@ -19,11 +20,22 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float MaxHealth = 100.f;
+	UPROPERTY(Replicated)
+	float CurrHealth;
+
+	virtual void GetLifetimeReplicatedProps( TArray< FLifetimeProperty > & OutLifetimeProps ) const override;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual float GetHealth_Implementation() override;
+	virtual void SetHealth_Implementation(float NewHealth) override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TEAMS Team;
+	virtual TEAMS GetTeam_Implementation() override;
 
 };
