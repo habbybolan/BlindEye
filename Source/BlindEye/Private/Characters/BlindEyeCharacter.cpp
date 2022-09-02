d17bda671dfb94d2ab9a7a1ab5a59e236ff1c833
pg22nicholas <97759807+pg22nicholas@users.noreply.github.com>
2022-09-01 17:37:44 -0700

    Player forward forcing to controller rotation

--- Source/BlindEye/Private/Characters/BlindEyeCharacter.cpp
+++ Source/BlindEye/Private/Characters/BlindEyeCharacter.cpp
@@ -23,11 +23,11 @@ ABlindEyeCharacter::ABlindEyeCharacter()
 
 	// Don't rotate when the controller rotates. Let that just affect the camera.
 	bUseControllerRotationPitch = false;
-	bUseControllerRotationYaw = false;
+	bUseControllerRotationYaw = true;
 	bUseControllerRotationRoll = false;
 
 	// Configure character movement
-	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
+	GetCharacterMovement()->bOrientRotationToMovement = false; // Character moves in the direction of input...	
 	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
 	GetCharacterMovement()->JumpZVelocity = 600.f;
 	GetCharacterMovement()->AirControl = 0.2f;
