0ac781fbd27bcdc87eeeb0464f015c77219436a7
pg22nicholas <pg22nicholas@vfs.com>
2022-09-04 14:55:54 -0700

    Added basic spawning implementation, classes hardcoded to server/client

--- Source/BlindEye/Private/Characters/BlindEyePlayerController.cpp
+++ Source/BlindEye/Private/Characters/BlindEyePlayerController.cpp
@@ -3,7 +3,44 @@
 
 #include "Characters/BlindEyePlayerController.h"
 
+#include "Characters/BlindEyeCharacter.h"
+#include "Gameplay/BlindEyeGameMode.h"
+#include "Kismet/GameplayStatics.h"
+
 void ABlindEyePlayerController::SER_SpawnPlayer_Implementation()
 {
-	// TODO: 
+	UWorld* world = GetWorld();
+	if (!world) return;
+
+	ABlindEyeGameMode* BlindEyeGameMode = Cast<ABlindEyeGameMode>(UGameplayStatics::GetGameMode(world));
+	if (BlindEyeGameMode == nullptr) return;
+
+	FActorSpawnParameters spawnParams;
+	spawnParams.Owner = this;
+	spawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+	FTransform spawnTransform = BlindEyeGameMode->GetSpawnPoint();
+	TSubclassOf<ABlindEyeCharacter> PlayerClassType;
+
+	// TODO: Temporary for test spawning
+	if (IsServer)
+	{
+		PlayerClassType = BlindEyeGameMode->PlayerClassTypes[0];
+		IsServer = !IsServer;
+	} else
+	{
+		PlayerClassType = BlindEyeGameMode->PlayerClassTypes[1];
+	}
+		
+	OwningCharacter = Cast<ABlindEyeCharacter>(world->SpawnActor<ABlindEyeCharacter>(PlayerClassType, spawnTransform, spawnParams));
+	if (OwningCharacter)
+	{
+		APawn* CachedPawn = GetPawn();
+		Possess(OwningCharacter);
+
+		if (CachedPawn != nullptr)
+		{
+			CachedPawn->Destroy();
+		}
+	}
 }
