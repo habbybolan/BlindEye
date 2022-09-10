ce142a60895bcdcdd4fac2832e30c15f11472ec0
pg22nicholas <pg22nicholas@vfs.com>
2022-09-09 23:18:35 -0700

    Added attacking check logic to snapper controller, added service to perform basic attack while moving

--- Source/BlindEye/Private/Enemies/SnapperEnemyController.cpp
+++ Source/BlindEye/Private/Enemies/SnapperEnemyController.cpp
@@ -6,6 +6,7 @@
 #include "Shrine.h"
 #include "BehaviorTree/BehaviorTree.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "Enemies/SnapperEnemy.h"
 #include "Kismet/GameplayStatics.h"
 
 void ASnapperEnemyController::BeginPlay()
@@ -27,6 +28,44 @@ void ASnapperEnemyController::BeginPlay()
 
 void ASnapperEnemyController::SetTargetEnemy(AActor* target)
 {
+	UBlackboardComponent* BlackboardComp = GetBlackboardComponent();
+	if (BlackboardComp == nullptr) return;
+	
 	GetBlackboardComponent()->SetValueAsObject(TEXT("EnemyActor"), target);
 	Target = MakeWeakObjectPtr<AActor>(target);
+}
+
+bool ASnapperEnemyController::CanBasicAttack()
+{
+	return !IsBasicAttackOnDelay;
+}
+
+bool ASnapperEnemyController::IsInBasicAttackRange()
+{
+	if (Target.IsValid())
+	{
+		FVector TargetLocation = Target->GetActorLocation();
+
+		if (!Snapper) return false;
+		return FVector::Distance(TargetLocation, Snapper->GetActorLocation()) < DistanceToBasicAttack;
+	}
+	return false;
+}
+
+void ASnapperEnemyController::PerformBasicAttack()
+{
+	Snapper->PerformBasicAttack();
+	IsBasicAttackOnDelay = true;
+	GetWorldTimerManager().SetTimer(BasicAttackDelayTimerHandle, this, &ASnapperEnemyController::SetCanBasicAttack, BasicAttackDelay, false);
+}
+
+void ASnapperEnemyController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+	Snapper = Cast<ASnapperEnemy>(InPawn);
+}
+
+void ASnapperEnemyController::SetCanBasicAttack()
+{
+	IsBasicAttackOnDelay = false;
 }
