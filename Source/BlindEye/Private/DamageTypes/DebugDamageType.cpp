22cd5e2ef69bb4196521b284f4d68debe8dc75ca
pg22nicholas <97759807+pg22nicholas@users.noreply.github.com>
2022-09-12 15:42:34 -0700

    Debug damage type created

---
+++ Source/BlindEye/Private/DamageTypes/DebugDamageType.cpp
@@ -0,0 +1,9 @@
+// Copyright (C) Nicholas Johnson 2022
+
+
+#include "DamageTypes/DebugDamageType.h"
+
+UDebugDamageType::UDebugDamageType() : UBaseDamageType()
+{
+	DebugDamageEverything = true;
+}
