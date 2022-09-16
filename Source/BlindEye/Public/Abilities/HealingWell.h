9b6732e7a1007c44ecff216aa605d07d894f3854
pg22nicholas <97759807+pg22nicholas@users.noreply.github.com>
2022-09-15 21:03:49 -0700

    created healing well

---
+++ Source/BlindEye/Public/Abilities/HealingWell.h
@@ -0,0 +1,45 @@
+// Copyright (C) Nicholas Johnson 2022
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HealingWell.generated.h"
+
+UCLASS()
+class BLINDEYE_API AHealingWell : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AHealingWell();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float HealPercentIncr = 1000;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float HealCheckDelay = 0.2f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float Radius = 450;
+ 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float Duration = 450;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void PerformHealCheck();
+	FTimerHandle HealingCheckTimerHandle;
+
+	void SpawnParticles();
+	void KillParticles();
+
+	virtual void BeginDestroy() override;
+
+};
