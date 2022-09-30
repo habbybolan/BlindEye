20f8e0976478ef3e2ecddc9cd9eef68c142ea947
pg22nicholas <97759807+pg22nicholas@users.noreply.github.com>
2022-09-29 17:48:12 -0700

    Updated spawn points so it doesn't break in packaged build

--- Source/BlindEye/Private/Enemies/Burrower/BurrowerSpawnPoint.cpp
+++ Source/BlindEye/Private/Enemies/Burrower/BurrowerSpawnPoint.cpp
@@ -9,14 +9,14 @@ ABurrowerSpawnPoint::ABurrowerSpawnPoint()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	
-	ArrowComponent = CreateEditorOnlyDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
-	if (ArrowComponent)
-	{
-		ArrowComponent->ArrowColor = FColor(150, 200, 255);
-		ArrowComponent->bTreatAsASprite = true;
-		ArrowComponent->SetupAttachment(RootComponent);
-		ArrowComponent->bIsScreenSizeScaled = true;
-	}
+	// ArrowComponent = CreateEditorOnlyDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
+	// if (ArrowComponent)
+	// {
+	// 	ArrowComponent->ArrowColor = FColor(150, 200, 255);
+	// 	ArrowComponent->bTreatAsASprite = true;
+	// 	ArrowComponent->SetupAttachment(RootComponent);
+	// 	ArrowComponent->bIsScreenSizeScaled = true;
+	// }
 }
 
 // Called when the game starts or when spawned
