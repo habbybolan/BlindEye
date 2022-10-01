f192d7da7814ba8f9b993371dac5f9f70b6dfe4b
pg22nicholas <pg22nicholas@vfs.com>
2022-09-30 23:20:32 -0700

    removed unnecessary particle logic of crow cocoon in code

--- Source/BlindEye/Private/Abilities/CrowCocoon.cpp
+++ Source/BlindEye/Private/Abilities/CrowCocoon.cpp
@@ -11,10 +11,6 @@
 ACrowCocoon::ACrowCocoon() : AAbilityBase()
 {
 	AbilityStates.Add(new UCrowCocoonStart(this));
-	AbilityStates.Add(new UCrowPulseState(this));
-	
-	PulseParticles.SetNum(MaxNumberPulses, false);
-	SpawnedPulseParticles.SetNum(MaxNumberPulses, false);
 	DamageTicks.SetNum(MaxNumberPulses);
 }
 
@@ -28,24 +24,14 @@ void ACrowCocoon::StartHoldLogic()
 	TimeHoldStart = GetGameTimeSinceCreation();
 }
 
-void ACrowCocoon::EndHold()
-{
-	float CurrTime = GetGameTimeSinceCreation();
-	GEngine->AddOnScreenDebugMessage(INDEX_NONE, 1.0f, FColor::Emerald, "Held for: " + FString::SanitizeFloat(CurrTime - TimeHoldStart));
-
-	UWorld* world = GetWorld();
-	if (!world) return;
-	world->GetTimerManager().ClearTimer(PulseTimerHandle);
-	MULT_PerformExplosionPulse();
-}
-
 void ACrowCocoon::EndAbilityLogic()
 {
 	Super::EndAbilityLogic();
-	if (SpawnedFullyChargedParticle) SpawnedFullyChargedParticle->Deactivate();
 
 	UWorld* world = GetWorld();
 	if (!world) return;
+	 
+	world->GetTimerManager().ClearTimer(PulseTimerHandle);
 
 	UGameplayStatics::ApplyPointDamage(GetInstigator(), 1, FVector::ZeroVector,
 		FHitResult(), GetInstigator()->GetController(), GetInstigator(), MainDamageType);
@@ -66,7 +52,6 @@ void ACrowCocoon::PerformPulse()
 		UGameplayStatics::ApplyRadialDamage(world, DamageTicks[CalcPulseIndex()], GetInstigator()->GetActorLocation(), Radius, FirstPulseDamageType,
 		TArray<AActor*>(), GetInstigator());
 	}
-	MULT_PerformPulseParticles(CalcPulseIndex());
 
 	// consume bird meter
 	if (!TryConsumeBirdMeter(CostPercentPerSec * MaxHoldDuration / MaxNumberPulses))
@@ -85,31 +70,6 @@ uint8 ACrowCocoon::CalcPulseIndex()
 	return FMath::Min(pulseIndex, LastIndex);
 }
 
-void ACrowCocoon::MULT_PerformExplosionPulse_Implementation()
-{
-	SpawnedExplosionPulse = UNiagaraFunctionLibrary::SpawnSystemAttached(ExplosionPulse, GetInstigator()->GetRootComponent(), NAME_None,
-		GetInstigator()->GetActorLocation(), FRotator::ZeroRotator, FVector::OneVector,
-		EAttachLocation::KeepWorldPosition, false, ENCPoolMethod::AutoRelease);
-}
-
-void ACrowCocoon::MULT_PerformPulseParticles_Implementation(uint8 pulseIndex) 
-{
-	SpawnedPulseParticles[pulseIndex] = UNiagaraFunctionLibrary::SpawnSystemAttached(PulseParticles[pulseIndex], GetInstigator()->GetRootComponent(), NAME_None,
-		GetInstigator()->GetActorLocation(), FRotator::ZeroRotator, FVector::OneVector,
-		EAttachLocation::KeepWorldPosition, false, ENCPoolMethod::AutoRelease);
-
-	// Play extra particle when last pulse reached
-	if (pulseIndex == MaxNumberPulses - 1)
-	{
-		SpawnedFullyChargedParticle = UNiagaraFunctionLibrary::SpawnSystemAttached(FullyChargedParticle, GetInstigator()->GetRootComponent(), NAME_None,
-		GetInstigator()->GetActorLocation(), FRotator::ZeroRotator, FVector::OneVector,
-		EAttachLocation::KeepWorldPosition, false, ENCPoolMethod::AutoRelease);
-
-		UWorld* world = GetWorld();
-		if (!world) return;
-		world->GetTimerManager().ClearTimer(PulseTimerHandle);
-	}
-}
 
 // **** States *******
 
@@ -138,7 +98,8 @@ void UCrowCocoonStart::RunState(EAbilityInputTypes abilityUsageType)
 	FAbilityState::RunState(abilityUsageType);
 	if (!Ability) return;
 	Ability->BP_AbilityStarted();
-	Ability->Blockers.IsMovementBlocked = true;
+	Ability->Blockers.IsMovementSlowBlocked = true;
+	Ability->Blockers.MovementSlowAmount = 0.2;
 	Ability->Blockers.IsOtherAbilitiesBlocked = true;
 	if (abilityUsageType == EAbilityInputTypes::Released)
 	{
@@ -154,36 +115,6 @@ void UCrowCocoonStart::RunState(EAbilityInputTypes abilityUsageType)
 }
 
 void UCrowCocoonStart::ExitState()
-{
-	FAbilityState::ExitState();
-	if (!Ability) return;
-	Ability->EndCurrState();
-	Ability->UseAbility(EAbilityInputTypes::None);
-}
-
-// Cocoon Pulse State *********************
-
-UCrowPulseState::UCrowPulseState(AAbilityBase* ability) : FAbilityState(ability) {}
-
-void UCrowPulseState::TryEnterState(EAbilityInputTypes abilityUsageType)
-{
-	FAbilityState::TryEnterState(abilityUsageType);
-	RunState();
-}
-
-void UCrowPulseState::RunState(EAbilityInputTypes abilityUsageType)
-{
-	FAbilityState::RunState(abilityUsageType);
-	// Perform explosion attack
-	if (!Ability) return;
-	if (ACrowCocoon* Cocoon = Cast<ACrowCocoon>(Ability))
-	{
-		Cocoon->EndHold();
-	}
-	ExitState();
-}
-
-void UCrowPulseState::ExitState()
 {
 	FAbilityState::ExitState();
 	if (!Ability) return;
