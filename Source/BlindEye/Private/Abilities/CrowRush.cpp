b1dc0993f145173abc0873781ac4795ad3bc5503
pg22nicholas <pg22nicholas@vfs.com>
2022-10-06 19:49:49 -0700

    Created initial crow rush class

---
+++ Source/BlindEye/Private/Abilities/CrowRush.cpp
@@ -0,0 +1,9 @@
+// Copyright (C) Nicholas Johnson 2022
+
+
+#include "Abilities/CrowRush.h"
+
+ACrowRush::ACrowRush()
+{
+	// TODO: Add States
+}
---
+++ Source/BlindEye/Public/Abilities/CrowRush.h
@@ -0,0 +1,20 @@
+// Copyright (C) Nicholas Johnson 2022
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/AbilityBase.h"
+#include "CrowRush.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLINDEYE_API ACrowRush : public AAbilityBase
+{
+	GENERATED_BODY()
+
+public:
+	ACrowRush();
+	
+};
