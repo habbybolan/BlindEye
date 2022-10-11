e467eed3fbced7bb26bd823c037d8a0ee5bdfd94
pg22nicholas <pg22nicholas@vfs.com>
2022-10-10 20:51:15 -0700

    readded knockback to crow flurry and cooldown on knockback for snapper

--- Source/BlindEye/Private/Enemies/Snapper/SnapperHealthComponent.cpp
+++ Source/BlindEye/Private/Enemies/Snapper/SnapperHealthComponent.cpp
@@ -7,12 +7,26 @@
 
 void USnapperHealthComponent::KnockBack(FVector KnockBackForce, AActor* DamageCause)
 {
-	Super::KnockBack(KnockBackForce, DamageCause);
+	UWorld* World = GetWorld();
+	if (World == nullptr) return;
 
+	// TODO: Only apply ragdoll delay to crow flurry?
+	if (!bCanKnockBack) return;
+
+	bCanKnockBack = false;
+	World->GetTimerManager().SetTimer(KnockBackCooldownTimer, this, &USnapperHealthComponent::KnockBackOffCooldown, KnockBackCooldown, false);
+	
+	Super::KnockBack(KnockBackForce, DamageCause);
+	
 	// prevent ragdoll if knockback not large enough
 	if (KnockBackForce.Size() < 500) return;
 	if (ASnapperEnemy* Snapper = Cast<ASnapperEnemy>(GetOwner()))
 	{
 		Snapper->TryRagdoll(true);
 	}
+}
+
+void USnapperHealthComponent::KnockBackOffCooldown()
+{
+	bCanKnockBack = true;
 }
