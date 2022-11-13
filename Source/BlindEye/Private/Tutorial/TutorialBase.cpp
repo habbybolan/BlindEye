fdbc2236143040bcfdcaee6f5bc94866a40cc62a
pg22nicholas <pg22nicholas@vfs.com>
2022-11-12 19:11:32 -0800

    Added some simple base tutorial logic and structure

--- Source/BlindEye/Private/Tutorial/TutorialBase.cpp
+++ Source/BlindEye/Private/Tutorial/TutorialBase.cpp
@@ -3,15 +3,20 @@
 
 #include "Tutorial/TutorialBase.h"
 
+#include "Gameplay/BlindEyeGameState.h"
+#include "Kismet/GameplayStatics.h"
+
 ATutorialBase::ATutorialBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
-
 }
 
 void ATutorialBase::SetupTutorial()
 {
 	bRunning = true;
+	UWorld* World = GetWorld();
+	BlindEyeGS = Cast<ABlindEyeGameState>(UGameplayStatics::GetGameState(World));
+	check(BlindEyeGS)
 }
 
 void ATutorialBase::EndTutorial()
--- Source/BlindEye/Public/Tutorial/TutorialBase.h
+++ Source/BlindEye/Public/Tutorial/TutorialBase.h
@@ -3,9 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "TutorialManager.h"
 #include "GameFramework/Actor.h"
-#include "Gameplay/BlindEyePlayerState.h"
 #include "TutorialBase.generated.h"
+
+class ABlindEyeGameState;
 
 UCLASS(Abstract)
 class BLINDEYE_API ATutorialBase : public AActor
@@ -14,12 +16,13 @@ class BLINDEYE_API ATutorialBase : public AActor
 	
 public:	
 	ATutorialBase();
-
+ 
 	virtual void SetupTutorial();
 	virtual void SetTutorialRunning() PURE_VIRTUAL(ATutorialBase::SetTutorialRunning,);
 	virtual void EndTutorial();
 
 	bool bRunning = false;
-	
-
+ 
+	UPROPERTY()
+	ABlindEyeGameState* BlindEyeGS;
 };
