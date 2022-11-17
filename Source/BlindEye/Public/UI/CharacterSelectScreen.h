3ff250f46a88fda5b857bfff33611b15402f8cbd
pg22nicholas <pg22nicholas@vfs.com>
2022-11-17 02:05:16 -0800

    Created empty map and class for character select logic

---
+++ Source/BlindEye/Public/UI/CharacterSelectScreen.h
@@ -0,0 +1,17 @@
+// Copyright (C) Nicholas Johnson 2022
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CharacterSelectScreen.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLINDEYE_API UCharacterSelectScreen : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
