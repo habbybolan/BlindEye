d74ae7447e1a92342c866e837242f7b395d0b059
pg22nicholas <pg22nicholas@vfs.com>
2022-11-30 20:39:49 -0800

    Hunter bar initializes after subscribed to hunter and healthbar percent starts at proper amount

--- Source/BlindEye/Private/HUD/HunterHealthbar.cpp
+++ Source/BlindEye/Private/HUD/HunterHealthbar.cpp
@@ -8,6 +8,8 @@ void UHunterHealthbar::SubscribeToHunter(AHunterEnemy* hunter)
 	Hunter = MakeWeakObjectPtr(hunter);
 	Hunter->OnDamageDelegate.AddDynamic(this, &UHunterHealthbar::HunterHealthUpdated);
 	UpdateHealth(Hunter->GetHealthPercent());
+
+	BP_StartAnimation();
 }
 
 void UHunterHealthbar::HunterHealthUpdated(float Damage, FVector HitLocation, const UDamageType* DamageType,
